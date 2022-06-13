ffa99b5780895e5d56ec7a8119ad5053d6cc540c
Juan-Romero <juandinarte11@gmail.com>
2022-06-13 17:39:59 -0300

    Exercicios das pecas finalizado

---
+++ introducao.js
@@ -0,0 +1,33 @@
+// console.log("Juan")
+
+// String ->"Juan" ou 'Juan' ou `Juan`
+
+// Number -> 15 ou 15.6
+
+// Object -> {
+//     nome:"Juan",
+//     idade: 20
+// }
+
+// console.log("5 + 5 é igual:", 5 + 5)
+// console.log("5 - 5 é igual:", 5 - 5)
+// console.log("5 * 5 é igual:", 5 * 5)
+// console.log("5 / 5 é igual:", 5 / 5)
+
+// console.log("O resto de 5 dividido por 2 é:", 5 % 2)
+
+// Incremento -> contador++
+// Decremento -> contador--
+
+// Concatenacao -> Juntar dois textos, duas strings
+// Interpolacao (usa crase ``), Template Strings -> Exibir valores de variaveis em uma string
+let primeiroNome = "Juan"
+let ultimoNome = "Ferreira"
+
+let nomeCompleto = primeiroNome + ultimoNome
+
+console.log("Primeiro nome:", primeiroNome) 
+console.log("Ultimo nome:", ultimoNome) 
+console.log("Nome Completo:", nomeCompleto) 
+
+console.log(`O primeiro nome do aluno eh ${primeiroNome} e o ultimo eh ${ultimoNome}`)
---
+++ pecas.js
@@ -0,0 +1,19 @@
+let pesoPeca = 50
+let numeroPecas = 7
+let nomePeca = "Roda"
+ 
+if(pesoPeca > 100) {
+    console.log("Pode-se cadastrar a peca")
+} else {
+    console.log("Peca com peso insuficiente")
+}
+if(numeroPecas >= 10) {
+    console.log("Impossivel cadastrar, numero de pecas excedido")
+} else {
+    console.log("Cadastro Permitido")
+}
+if(nomePeca.length < 3) {
+    console.log("Numero de caracteres insuficiente")
+} else {
+    console.log("Nome adequado de acordo com as diretivas")
+}
